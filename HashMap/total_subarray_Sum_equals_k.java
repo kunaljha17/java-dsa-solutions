60f521a28274c79e715fbad346ad5c29d62abdd6
KUNAL KUMAR <jhakunal124@gmail.com>
2026-07-11 00:00:27 +0530

    Update total_subarray_Sum_equals_k.java

--- HashMap/total_subarray_Sum_equals_k.java
+++ HashMap/total_subarray_Sum_equals_k.java
@@ -1,3 +1,10 @@
+// Subarray Sum = Current Prefix − Previous Prefix
+
+// k = currentPrefix − previousPrefix
+
+// previousPrefix = currentPrefix − k
+
+
 import java.util.HashMap;
 
 public class total_subarray_Sum_equals_k {
@@ -20,3 +27,36 @@ public class total_subarray_Sum_equals_k {
         System.out.println("Number of subarray sum of " + k + " is : " + subarrays);
     }
 }
+
+
+
+
+// 1. Use Prefix Sum + HashMap.
+
+// 2. HashMap stores:
+//    prefixSum -> frequency
+
+// 3. Initialize:
+//    map.put(0, 1)
+//    (handles subarrays starting from index 0)
+
+// 4. For each element:
+//    sum += nums[i]
+
+// 5. Search:
+//    if(map contains (sum - k))
+//        ans += map.get(sum - k)
+
+// 6. Why sum-k?
+//    currentPrefix - previousPrefix = k
+//    => previousPrefix = currentPrefix - k
+
+// 7. After searching, store current prefix:
+//    map.put(sum, map.getOrDefault(sum, 0) + 1)
+
+// 8. Never store (sum-k), only store current sum.
+
+// 9. Frequency is important because the same prefix sum may occur multiple times.
+
+// 10. Time: O(n)
+//     Space: O(n)
