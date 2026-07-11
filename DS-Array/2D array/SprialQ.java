628464d84a9b85097932e9b2ecea364e6160bb8a
KUNAL KUMAR <jhakunal124@gmail.com>
2026-07-11 14:30:08 +0530

    Update SprialQ.java

--- DS-Array/2D array/SprialQ.java
+++ DS-Array/2D array/SprialQ.java
@@ -77,3 +77,102 @@ public class SprialQ {
         System.out.println(diagonalSum(matrix));
     }
 }
+
+
+
+
+
+
+
+
+//MY versionn Approach 1
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length ;
+        int endCol = matrix[0].length ;
+        List<Integer> list = new ArrayList<>();
+
+        while (startRow < endRow && startCol < endCol) {
+            if(startRow==endRow-1){
+                for(int i = startCol;i<endCol;i++){
+                    list.add(matrix[startRow][i]);
+                }
+                break;
+            }
+            if(startCol==endCol-1){
+                for(int i = startRow;i<endRow;i++){
+                    list.add(matrix[i][startCol]);
+                }
+                break;
+            }
+            for (int i = startCol; i < endCol-1; i++) {
+                list.add(matrix[startRow][i]);
+            }
+            for (int i = startRow; i < endRow-1; i++) {
+                list.add(matrix[i][endCol-1]);
+            }
+            for (int i = endCol-1; i >startCol; i--) {
+                list.add(matrix[endRow-1][i]);
+            }
+            for (int i = endRow-1; i > startRow; i--) {
+                list.add(matrix[i][startCol]);
+            }
+
+            startRow++;
+            startCol++;
+            endRow--;
+            endCol--;
+        }
+        return list;
+    }
+}
+
+
+
+//Approach 2 for spiral matrix
+public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> list = new ArrayList<>();
+
+        int startRow = 0;
+        int endRow = matrix.length - 1;
+        int startCol = 0;
+        int endCol = matrix[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+
+            // Top Row
+            for (int i = startCol; i <= endCol; i++) {
+                list.add(matrix[startRow][i]);
+            }
+
+            // Right Column
+            for (int i = startRow + 1; i <= endRow; i++) {
+                list.add(matrix[i][endCol]);
+            }
+
+            // Bottom Row
+            if (startRow < endRow) {
+                for (int i = endCol - 1; i >= startCol; i--) {
+                    list.add(matrix[endRow][i]);
+                }
+            }
+
+            // Left Column
+            if (startCol < endCol) {
+                for (int i = endRow - 1; i > startRow; i--) {
+                    list.add(matrix[i][startCol]);
+                }
+            }
+
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+        }
+
+        return list;
+    }
