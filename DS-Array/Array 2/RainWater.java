b55991eac3b7c7046bf997613b0705c49a4393f0
KUNAL KUMAR <jhakunal124@gmail.com>
2026-07-12 13:13:43 +0530

    Update RainWater.java

--- DS-Array/Array 2/RainWater.java
+++ DS-Array/Array 2/RainWater.java
@@ -1,3 +1,6 @@
+
+//This approach solvere this problem with O(n) space and time complexity
+// not Optimize or standard approach
 public class RainWater {
 
     public static int RainWater_trapped(int height[]) {
@@ -21,6 +24,48 @@ public class RainWater {
         for (int i = 0; i < n; i++) {
             int waterLevel = Math.min(RightMaxBound[i], leftMaxBound[i]);
             trappedWater = trappedWater + (waterLevel - height[i]);
+        }
+        return trappedWater;
+    }
+
+
+
+
+// 1. Keep two pointers: left and right.
+// 2. Maintain leftMax and rightMax.
+// 3. Compare leftMax and rightMax.
+// 4. Smaller max decides the water level.
+// 5. If leftMax <= rightMax, process left.
+// 6. Else, process right.
+// 7. Update max if current bar is taller.
+// 8. Otherwise add (max - current height).
+// 9. Move only the processed pointer.
+// 10. Time O(n), Space O(1).
+
+   
+  public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int trappedWater = 0;
+        while (left <= right) {
+            if (leftMax <= rightMax) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    trappedWater += leftMax - height[left];
+                }
+
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    trappedWater += rightMax - height[right];
+                }
+                right--;
+            }
         }
         return trappedWater;
     }
