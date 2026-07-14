446bb6e0ddc0c00e6e1cec6152151fda1e2eeba5
KUNAL KUMAR <jhakunal124@gmail.com>
2026-07-14 21:23:02 +0530

    Update String_compression.java

--- String/String_compression.java
+++ String/String_compression.java
@@ -1,33 +1,46 @@
+//Approach 1 
+//2 pointer approach 
+//store previous char 
+//read with i next char 
+//if equal ith == prevChar  , just count++
+//if not equal then , store into input array at start id , we actaully need jth pointer or charsIdx to write original chars[] 
+//then , check count >1 then store in array as single num in [] so make string then so on
+//rest count = 1 and update prevChar now 
+//at last we only store count of last group char , not write in chars .. so using j write pointer write chars last group
+// return charsIdx or j as a result .. 
+
+
 class Solution {
     public int compress(char[] chars) {
-        int j = 0;
-        int count = 1;
-        char prevChar = chars[0];
-        for (int i = 1; i < chars.length; i++) {
-            char current = chars[i];
-            if (current == prevChar) {
-                count++;
-            } else {
-                chars[j++] = prevChar;
-                if (count > 1) {
-                    String countStr = String.valueOf(count);
-                    for (int k = 0; k < countStr.length(); k++) {
-                        chars[j++] = countStr.charAt(k);
-                    }
+       int count = 1;
+       char prevChar = chars[0];
+       int charsIdx = 0;
+       for(int i = 1;i<chars.length;i++){
+        if(chars[i] == prevChar){
+            count++;
+        }else{
+            chars[charsIdx++] = prevChar;
+            //count
+            if(count>1){
+                String countStr = Integer.toString(count);
+                for(int k = 0;k<countStr.length();k++){
+                    chars[charsIdx++] = countStr.charAt(k);
                 }
-                prevChar = current;
-                count = 1;
             }
+            count = 1;
+            prevChar = chars[i];
         }
-        chars[j++] = prevChar;
-        if (count > 1) {
-            String countStr = String.valueOf(count);
-            for (int k = 0; k < countStr.length(); k++) {
-                chars[j++] = countStr.charAt(k);
-            }
-        }
-        return j;
+       }
+       // for last group
+       chars[charsIdx++] = prevChar;
+       if(count>1){
+         String countStr = Integer.toString(count);
+         for(int k = 0;k<countStr.length();k++){
+          chars[charsIdx++] = countStr.charAt(k);
+       }
     }
+    return charsIdx;
+ }
 }
 
 
