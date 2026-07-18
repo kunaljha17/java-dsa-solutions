f1a1b32d94cf09af78ddea2ec634719f228d73bf
KUNAL KUMAR <jhakunal124@gmail.com>
2026-07-18 16:28:36 +0530

    Update Median_of_Two_Sorted_Arrays.java

--- String /Median_of_Two_Sorted_Arrays.java
+++ String /Median_of_Two_Sorted_Arrays.java
@@ -1,4 +1,5 @@
 // idea (merge first, then find the median)
+//Approach 1
 
 class Solution {
     public void merge(int arr[],int nums1[] , int nums2[]){
@@ -32,5 +33,55 @@ class Solution {
             return arr[n / 2];
             }
          return (arr[n / 2 - 1] + arr[n / 2]) / 2.0;
+    }
+}
+
+
+
+//Approach 2 
+
+// first take smaller array
+// then take cut1 .. 
+// as same we want leftsize - cut1 for cut2 of another array 
+// then check L1<=R2 and L2<=R1 if true both then check n is odd or even if odd then return max of l1 l2 
+// if even then take avg (max(L1,L2) + min(R1,R2)) / 2
+// if false then  check that L1>R2 then high = cut1-1
+// if L2>R1 then low = cut1+1
+// at then end we get median ...
+
+class Solution {
+   
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        if(nums1.length >nums2.length){
+            return findMedianSortedArrays(nums2 , nums1);
+        }
+        int low = 0;
+        int high = n;// let smaller is a nums1
+        int leftsize = (n+m+1)/2;
+        while(low<=high){
+            int cut1 = low +(high-low)/2;
+            int cut2 = leftsize - cut1;
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int r1 = (cut1 == nums1.length) ? Integer.MAX_VALUE : nums1[cut1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            int r2 = (cut2 == nums2.length) ? Integer.MAX_VALUE : nums2[cut2];
+            if(l1<=r2 && l2<=r1){
+                   //check length of both array nums1 and nums2 
+                if((n + m) % 2 != 0){ // if even 
+                    return (double)Math.max(l1,l2);
+                }else{ // odd
+                    return (double)(Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                }
+            }
+            else if(l1>r2){
+                high = cut1-1;
+            }
+            else if(l2>r1){
+                low = cut1+1;
+            }
+        }
+        return 0;
     }
 }
