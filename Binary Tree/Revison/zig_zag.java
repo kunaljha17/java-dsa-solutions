7f1ba23bfab7acf9bb667415d24b26bc57abcc88
KUNAL KUMAR <jhakunal124@gmail.com>
2026-08-06 20:45:17 +0530

    Create zig_zag.java

---
+++ Binary Tree/Revison/zig_zag.java
@@ -0,0 +1,38 @@
+
+
+
+
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            Deque<Integer> levelDeque = new ArrayDeque<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+
+                if (leftToRight) {
+                    levelDeque.addLast(node.val);
+                } else {
+                    levelDeque.addFirst(node.val);
+                }
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+            }
+
+            result.add(new ArrayList<>(levelDeque));
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+    }
+}
